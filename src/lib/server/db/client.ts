3d7dc1f7f050f5e43809d0a152cdde242d0efba2
jdglesener <89934737+jdglesener@users.noreply.github.com>
2026-05-08 19:39:13 -0700

    database error logging

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -3,6 +3,8 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
 const queryClient = postgres(env.DATABASE_URL, {
 	max: 10,
 	idle_timeout: 20,
